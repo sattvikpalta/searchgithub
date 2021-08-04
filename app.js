a4eba3ec075f2d5e76f5dcdeea2beb9143d7fded
Sattvik Palta <sattvikpalta@gmail.com>
2021-08-04 17:13:29 +0530

    Added repos to card

--- app.js
+++ app.js
@@ -25,10 +25,23 @@ async function getUser(username) {
     } = await axios(APIURL + username);
 
     createUserCard(data);
+    getRepos(username);
   } catch (error) {
     if (error.response.status === 404) {
       createErrorCard('No profile with the given username');
     }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const {
+      data
+    } = await axios(APIURL + username + '/repos?sort=created');
+
+    addReposToCard(data);
+  } catch (error) {
+    createErrorCard('Problem fetching repos');
   }
 }
 
@@ -61,6 +74,22 @@ function createErrorCard(msg) {
       <h1>${msg}</h1>
     </div> 
   `;
+}
+
+function addReposToCard(repos) {
+  const reposElement = document.getElementById('repos');
+
+  repos
+    .slice(0, 10)
+    .forEach(repo => {
+      const repoElement = document.createElement('a');
+      repoElement.classList.add('repo');
+      repoElement.href = repo.html_url;
+      repoElement.target = '_blank';
+      repoElement.innerText = repo.name;
+
+      reposElement.appendChild(repoElement);
+    });
 }
 
 formUser.addEventListener('submit', (e) => {
